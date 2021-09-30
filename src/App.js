987e90eec692d199a548a63c1efac4a5dc38ed1a
qualv13 <71326761+qualv13@users.noreply.github.com>
2021-09-30 20:48:12 +0200

    okay, it looks nice rn
    2:11:52 - props children

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/layout/MainNavigation";
 
 function App() {
   // localhost:3000/
@@ -10,6 +11,7 @@ function App() {
 
   return (
     <div>
+    <MainNavigation />
       <Switch>
         <Route path="/" exact={true} /*albo samo exact */>
           <AllMeetupsPage />
