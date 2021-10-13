81984447115a194b0c563e442fee1237bbd1f71a
qualv13 <71326761+qualv13@users.noreply.github.com>
2021-10-13 23:28:06 +0200

    yup

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,13 @@
 import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    function addMeetupHandler(meetupData) {
+        fetch('https://firstreactpage-default-rtdb.firebaseio.com/');
+    }
+
     return <section>
         <h1>Add new meetup</h1>
-        <NewMeetupForm />
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
     </section>
 }
 
