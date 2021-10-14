58b933d600b4c5a918ae6a504ae02c6f3d36eac0
qualv13 <71326761+qualv13@users.noreply.github.com>
2021-10-14 11:28:40 +0200

    somehow it works

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,14 +1,31 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
-    function addMeetupHandler(meetupData) {
-        fetch('https://firstreactpage-default-rtdb.firebaseio.com/');
-    }
+  const history = useHistory();
 
-    return <section>
-        <h1>Add new meetup</h1>
-        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+  function addMeetupHandler(meetupData) {
+    fetch("https://firstreactpage-default-rtdb.firebaseio.com/meetups.json",
+    {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    }
+    ).then(() => {
+        history.replace('/');
+    });
+    
+  }
+
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
+  );
 }
 
 export default NewMeetupPage;
